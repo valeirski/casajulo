6f035cc4fa88364a619c48055551f0cd680413d6
valeirski <info@vbworks.net>
2019-01-04 16:33:06 +0100

    edited instagram feed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,26 +3,12 @@ $(document).ready(function() {
 
     var userFeed = new Instafeed({
         get: 'user',
-        userId: '7866248133',
-        limit: 12,
+        userId: '5860909318',
+        limit: 9,
         resolution: 'standard_resolution',
-        accessToken: '7866248133.85f8b36.afb23b2534e041d18279f8519ddefb4f',
+        accessToken: '5860909318.04f11af.950dbf64df924bf2a6e6176ba9ab422f',
         sortBy: 'most-recent',
-        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        template: '<div class="col-lg-4 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
-
-
     userFeed.run();
-
-
-    // This will create a single gallery from all elements that have class "gallery-item"
-    $('.gallery').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
-    });
-
-
 });
