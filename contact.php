ae93ff33e3b1732a3f2078e86493120a11031d1b
Valéry Baelus <you@example.comvaleir@gmail.com>
2019-02-18 16:43:15 +0100

    edited contact form

--- contact.php
+++ contact.php
@@ -1,14 +1,14 @@
 <html>
     <head>
-        <meta http-equiv="refresh" content="2;url=http://www.bmc-betonwerken.be/" />
+        <meta http-equiv="refresh" content="2;url=http://www.casajulo.com/" />
     </head>
     <body>
       <?php
           ini_set( 'display_errors', 1 );
           error_reporting( E_ALL );
           $from = $_POST["email"];
-          $to = "wouter.mc@hotmail.com";
-          $subject = "contact formulier bmc betonwerken " . $_POST["name"];
+          $to = "info@broodjeskoerier.be";
+          $subject = "contact formulier casajulo" . $_POST["name"];
           $message = $_POST["address"] . "\r\n" . $_POST["message"];
           $headers = "From:" . $from;
           if(mail($to,$subject,$message, $headers)) {
