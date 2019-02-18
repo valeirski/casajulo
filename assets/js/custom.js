61b2f9f9dcb0453c2a57a648d4192c8731672308
Valéry Baelus <you@example.comvaleir@gmail.com>
2019-02-18 16:02:55 +0100

    added english translation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
         resolution: 'standard_resolution',
         accessToken: '5860909318.04f11af.950dbf64df924bf2a6e6176ba9ab422f',
         sortBy: 'most-recent',
-        template: '<div class="col-lg-4 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+
+        template: '<div class="col-lg-4 col-md-4 col-sm-2 col-xs-1 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
     userFeed.run();
 });
--- contact.php
+++ contact.php
@@ -1,14 +1,14 @@
 <html>
     <head>
-        <meta http-equiv="refresh" content="2;url=http://www.casa-julo.com/" />
+        <meta http-equiv="refresh" content="2;url=http://www.bmc-betonwerken.be/" />
     </head>
     <body>
       <?php
           ini_set( 'display_errors', 1 );
           error_reporting( E_ALL );
           $from = $_POST["email"];
-          $to = "broodjeskoerier@gmail.com";
-          $subject = "contact formulier casa julo " . $_POST["name"];
+          $to = "wouter.mc@hotmail.com";
+          $subject = "contact formulier bmc betonwerken " . $_POST["name"];
           $message = $_POST["address"] . "\r\n" . $_POST["message"];
           $headers = "From:" . $from;
           if(mail($to,$subject,$message, $headers)) {
